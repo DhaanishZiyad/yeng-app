d95ca3ce484293abb58af41e30eaaa48975375b4
Ismail Dhaanish Ziyad <dhaanishziyad@gmail.com>
2025-01-21 22:41:20 +0530

    payment fix 8

--- routes/web.php
+++ routes/web.php
@@ -108,8 +108,8 @@ Route::prefix('store')->middleware(['auth', 'verified'])->group(function () {
 
 });
 
-    
 Route::get('stripe-payment', [StoreController::class, 'stripePayment'])->name('store.stripe-payment');
+
 Route::get('stripe-success', [StoreController::class, 'stripeSuccess'])->name('store.stripe-success');
 
 
